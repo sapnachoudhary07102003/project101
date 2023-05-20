6cc03794d737162b2ce1a30a84a8a0a471e26f62
sapnachoudhary07102003 <69847056+sapnachoudhary07102003@users.noreply.github.com>
2023-05-20 11:12:49 +0530

    Update roll_a_dice.py

--- roll_a_dice.py
+++ roll_a_dice.py
@@ -3,39 +3,41 @@ import random
 def dice (ask):
     while ask=="y":
             no=random.randint(1,6)
-            print(no)
+            print(no) 
+            if(no==1):
+                 print("_ _ _")
+                 print("_ 0 _")
+                 print("_ _ _")
+
+            elif(no==2):
+                 print("0 _ _")
+                 print("_ _ _")
+                 print("_ _ 0")
+        
+            elif(no==3):
+                 print("0 _ _")
+                 print("_ 0 _")
+                 print("_ _ 0")
+
+            elif(no==4):
+                 print("0 _ 0")
+                 print("_ _ _")
+                 print("0 _ 0")
+
+            elif(no==5):
+                 print("0 _ 0")
+                 print("_ 0 _")
+                 print("0 _ 0")
+
+            elif(no==6):
+                 print("0 _ 0")
+                 print("0 _ 0")
+                 print("0 _ 0")
+        
             break
     
-    if(no==1):
-        print("_ _ _")
-        print("_ 0 _")
-        print("_ _ _")
-
-    elif(no==2):
-        print("0 _ _")
-        print("_ _ _")
-        print("_ _ 0")
-        
-    elif(no==3):
-        print("0 _ _")
-        print("_ 0 _")
-        print("_ _ 0")
-
-    elif(no==4):
-        print("0 _ 0")
-        print("_ _ _")
-        print("0 _ 0")
-
-    elif(no==5):
-        print("0 _ 0")
-        print("_ 0 _")
-        print("0 _ 0")
-
-    elif(no==6):
-        print("0 _ 0")
-        print("0 _ 0")
-        print("0 _ 0")
-    
+    while ask=="n":
+        again(ask)
 
     again(ask)
 
